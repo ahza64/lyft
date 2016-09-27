9011b232e84960885242013ac836b809db94e039
ahza64 <ahza64@yahoo.com>
2016-09-27 15:54:36 -0700

    update dependencies

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/', function (req, res) {
 });
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.MONGODB_URI || 3000, function () {
     console.log('Lyft app listening at http://localhost:3000/');
   });
